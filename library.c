b17bd2c8ebf060f114d4bcc3b6ad222d3725ae61
Kelvin <76610777+Kelvinlby@users.noreply.github.com>
2024-10-14 21:38:25 +0800

    update

--- library.c
+++ library.c
@@ -3,23 +3,8 @@
 #include <stdio.h>
 #include <sys/sysinfo.h>
 
-/************************************ message to ECHO HELLO WORLD ************************************
- *
- * Since this is a shared library, u need to create a separate C project to test its functionality.
- * Don't put test code in this repo, and it's preferred to use Dart to test it, but C is also OK.
- * And, Dart ffi interface only support functions that returns the following types:
- *      - Int / Int8 / Int16 / Int32 / Int64
- *      - Long / LongLong
- *      - Uint / Uint8 / Uint16 / Uint32 / Uint64
- *
- * So if a function that is designed to be called by `Submission` project, it should return values
- * in the types above.
- *
- * By the way, remove this message if u have read it.   :)
- */
 
-
-__u_long getTotalRam(void) {
+ULONG getTotalRam(void) {
     struct sysinfo info;
 
     if (sysinfo(&info) != 0) {
@@ -31,7 +16,7 @@ __u_long getTotalRam(void) {
 }
 
 
-__u_long getFreeRam(void) {
+ULONG getFreeRam(void) {
     struct sysinfo info;
 
     if (sysinfo(&info) != 0) {
@@ -43,7 +28,7 @@ __u_long getFreeRam(void) {
 }
 
 
-__uint8_t getRamUsage(void) {
+UINT8 getRamUsage(void) {
     struct sysinfo info;
 
     if (sysinfo(&info) != 0) {
@@ -51,7 +36,5 @@ __uint8_t getRamUsage(void) {
         return 1;
     }
 
-    // return 1 - (double) (info.freeram * info.mem_unit) / (double) (info.totalram * info.mem_unit);
-    // TODO scale the return.
-    return 0;
+    return (UINT8) (100(1 - (double) (info.freeram * info.mem_unit) / (double) (info.totalram * info.mem_unit)));
 }
--- library.h
+++ library.h
@@ -1,8 +1,21 @@
 #ifndef GPUMONITOR_LIBRARY_H
 #define GPUMONITOR_LIBRARY_H
 
-unsigned long getTotalRam(void);
-double getRamUsage(void);
-double getRamUsage(void);
+#include <bits/types.h>
+
+#define UINT8 __uint8_t
+#define UINT16 __uint16_t
+#define UINT32 __uint32_t
+#define UINT64 __uint64_t
+#define ULONG __u_long
+
+#define INT8 __int8_t
+#define INT16 __int16_t
+#define INT32 __int32_t
+#define INT64 __int64_t
+
+ULONG getTotalRam(void);
+ULONG getFreeRam(void);
+UINT8 getRamUsage(void);
 
 #endif //GPUMONITOR_LIBRARY_H
