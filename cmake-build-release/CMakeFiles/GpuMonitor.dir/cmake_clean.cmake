003a9713d67338d352f921d63130e9f491d348b9
Kelvin <76610777+Kelvinlby@users.noreply.github.com>
2024-10-14 17:32:32 +0800

    update

---
+++ cmake-build-release/CMakeFiles/GpuMonitor.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GpuMonitor.dir/library.c.o"
+  "CMakeFiles/GpuMonitor.dir/library.c.o.d"
+  "libGpuMonitor.pdb"
+  "libGpuMonitor.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/GpuMonitor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
