cef383d1b222e49809603f0898fe66b338150c3d
Patrick_Chen <patrickechohelloworld@outlook.com>
2024-10-16 11:37:22 +0800

    Remove rsmi status check

--- amd/vram_info.c
+++ amd/vram_info.c
@@ -9,11 +9,7 @@ UINT64 amdTotalVram(void) {
     rsmi_init(0);
 
     UINT64 total = 0;
-    rsmi_status_t ret = rsmi_dev_memory_total_get(0, RSMI_MEM_TYPE_VRAM, &total);
-
-    if (ret != RSMI_STATUS_SUCCESS) {
-        return 1;
-    }
+    rsmi_dev_memory_total_get(0, RSMI_MEM_TYPE_VRAM, &total);
 
     UINT64 total_buf = total;
     rsmi_shut_down();
@@ -28,12 +24,8 @@ UINT64 amdFreeVram(void) {
     rsmi_init(0);
 
     UINT64 used = 0, free = 0, total = 0;
-    rsmi_status_t ret1 = rsmi_dev_memory_total_get(0, RSMI_MEM_TYPE_VRAM, &total);
-    rsmi_status_t ret2 = rsmi_dev_memory_usage_get(0, RSMI_MEM_TYPE_VRAM, &used);
-
-    if (ret1 != RSMI_STATUS_SUCCESS || ret2 != RSMI_STATUS_SUCCESS) {
-        return 1;
-    }
+    rsmi_dev_memory_total_get(0, RSMI_MEM_TYPE_VRAM, &total);
+    rsmi_dev_memory_usage_get(0, RSMI_MEM_TYPE_VRAM, &used);
 
     free = used - total;
     rsmi_shut_down();
@@ -49,12 +41,8 @@ UINT8 amdVramUsage(void) {
 
     UINT8 percentage = 0;
     UINT64 used = 0, total = 0;
-    rsmi_status_t ret1 = rsmi_dev_memory_total_get(0, RSMI_MEM_TYPE_VRAM, &total);
-    rsmi_status_t ret2 = rsmi_dev_memory_usage_get(0, RSMI_MEM_TYPE_VRAM, &used);
-
-    if (ret1 != RSMI_STATUS_SUCCESS || ret2 != RSMI_STATUS_SUCCESS) {
-        return 1;
-    }
+    rsmi_dev_memory_total_get(0, RSMI_MEM_TYPE_VRAM, &total);
+    rsmi_dev_memory_usage_get(0, RSMI_MEM_TYPE_VRAM, &used);
 
     percentage = (UINT8) (100 * (FLOAT32) used / (FLOAT32) total);
     rsmi_shut_down();
