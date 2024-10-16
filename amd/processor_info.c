2b97b083caa442814bdaff0244384b2d62e077fc
Patrick_Chen <patrickechohelloworld@outlook.com>
2024-10-16 11:47:58 +0800

    Add utilization rate function for AMD gpu

--- amd/processor_info.c
+++ amd/processor_info.c
@@ -2,4 +2,17 @@
 #include "./rocm_smi.h"
 
 
+/** Get utilization of amd gpu
+ * @return percentage of Cuda core utilization
+ */
+UINT32 amdGpuUsage(void){
+    rsmi_init(0)
 
+    UINT32 useage;
+    rsmi_dev_busy_percent_get(0, &useage);
+
+    UINT32 useage_buf = useage;
+    rsmi_shut_down();
+
+    return useage;
+}
