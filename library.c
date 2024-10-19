f5819cbdf6a84dcff223fe5f4ad51de708e8fd17
Patrick_Chen <patrickechohelloworld@outlook.com>
2024-10-19 12:12:23 +0800

    Available & Cached Memory added

--- library.c
+++ library.c
@@ -1,6 +1,7 @@
 #include "library.h"
 
 #include <stdio.h>
+#include <string.h>
 #include <sys/sysinfo.h>
 #include <cuda_runtime.h>
 #include "./amd/rocm_smi.h"
@@ -58,4 +59,60 @@ UINT64 getFreeRam(void) {
     }
 
     return info.freeram * info.mem_unit;
+}
+
+
+/** Get cached memory size
+ * @return cached RAM in bytes
+ */
+UINT64 getCached() {
+    FILE *f = fopen("/proc/meminfo", "r");
+    if (f == NULL) {
+        perror("Error opening /proc/meminfo");
+    }
+    char line[256];
+    UINT64 cached = 0;
+
+    while (fgets(line, sizeof(line), f)) {
+        if (strncmp(line, "Cached:", 7) == 0) {
+            sscanf(line + 7, "%ld", &cached);
+            break;
+        }
+    }
+    fclose(f);
+    return cached * 1024;
+}
+
+
+/** Get available memory size
+ * @return available RAM in bytes
+ */
+UINT64 getAvailable(void) {
+    struct sysinfo info;
+
+    if (sysinfo(&info) != 0) {
+        perror("sysinfo");
+        return 1;
+    }
+
+    FILE *f = fopen("/proc/meminfo", "r");
+    if (f == NULL) {
+        perror("Error opening /proc/meminfo")
+        return 2;
+    }
+
+    char line[256];
+    UINT64 cached = 0;
+
+    while (fgets(line, sizeof(line), f)) {
+        if (strncmp(line, "Cached:", 7) == 0) {
+            sscanf(line + 7, "%ld", &cached);
+            break;
+        }
+    }
+    fclose(f);
+
+    UINT64 free = info.freeram * info.mem_unit;
+    UINT64 available = free + cached * 1024;
+    return available;
 }
