76bb5849761266b364cc266640a10d475af930eb
Patrick_Chen <patrickechohelloworld@outlook.com>
2024-11-05 23:21:59 +0800

    Nothing

---
+++ cmake-build-debug/CMakeFiles/GpuMonitor.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/patrick/application/clion-2024.2.3/Projects/GpuMonitor/amd/processor_info.c" "CMakeFiles/GpuMonitor.dir/amd/processor_info.c.o" "gcc" "CMakeFiles/GpuMonitor.dir/amd/processor_info.c.o.d"
+  "/home/patrick/application/clion-2024.2.3/Projects/GpuMonitor/amd/vram_info.c" "CMakeFiles/GpuMonitor.dir/amd/vram_info.c.o" "gcc" "CMakeFiles/GpuMonitor.dir/amd/vram_info.c.o.d"
+  "/home/patrick/application/clion-2024.2.3/Projects/GpuMonitor/library.c" "CMakeFiles/GpuMonitor.dir/library.c.o" "gcc" "CMakeFiles/GpuMonitor.dir/library.c.o.d"
+  "/home/patrick/application/clion-2024.2.3/Projects/GpuMonitor/nvidia/processor_info.c" "CMakeFiles/GpuMonitor.dir/nvidia/processor_info.c.o" "gcc" "CMakeFiles/GpuMonitor.dir/nvidia/processor_info.c.o.d"
+  "/home/patrick/application/clion-2024.2.3/Projects/GpuMonitor/nvidia/vram_info.c" "CMakeFiles/GpuMonitor.dir/nvidia/vram_info.c.o" "gcc" "CMakeFiles/GpuMonitor.dir/nvidia/vram_info.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/GpuMonitor.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GpuMonitor.dir/amd/processor_info.c.o"
+  "CMakeFiles/GpuMonitor.dir/amd/processor_info.c.o.d"
+  "CMakeFiles/GpuMonitor.dir/amd/vram_info.c.o"
+  "CMakeFiles/GpuMonitor.dir/amd/vram_info.c.o.d"
+  "CMakeFiles/GpuMonitor.dir/library.c.o"
+  "CMakeFiles/GpuMonitor.dir/library.c.o.d"
+  "CMakeFiles/GpuMonitor.dir/nvidia/processor_info.c.o"
+  "CMakeFiles/GpuMonitor.dir/nvidia/processor_info.c.o.d"
+  "CMakeFiles/GpuMonitor.dir/nvidia/vram_info.c.o"
+  "CMakeFiles/GpuMonitor.dir/nvidia/vram_info.c.o.d"
+  "libGpuMonitor.pdb"
+  "libGpuMonitor.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/GpuMonitor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
